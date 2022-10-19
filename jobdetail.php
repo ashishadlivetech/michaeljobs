1b74d407e1028218e1f8e68fd5587f50ee636d71
developer <developertestnew@gmail.com>
2022-10-19 23:04:54 +0530

    sdsd

--- jobdetail.php
+++ jobdetail.php
@@ -33,7 +33,7 @@ $api = $auth_val['api'];
 
 $curl = curl_init();
 curl_setopt_array($curl, array(
-  CURLOPT_URL => $api."jobs/".$post,
+  CURLOPT_URL => $api."jobs/".$_POST['id'],
   CURLOPT_RETURNTRANSFER => true,  
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
